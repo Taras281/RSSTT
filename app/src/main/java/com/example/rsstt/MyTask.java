b15006c68e81444bd902723de5b002f5e4a07fc7
Ivanov Taras <taras281@yandex.ru>
2021-03-03 21:36:18 -0800

    Уточнили работу ServiseNotife  - добавили поточек и уточнили работу с прошлым списком Устранил нарастающее колличество потоков Уточнили уведомления  - BIGTITLE Добавили сохранение настроек Добавили периодичность запроса лент Улучшили алгоритм поиска лент - теперь на странице ищется рабочая ссылка на RSS ленту и проверяется

--- app/src/main/java/com/example/rsstt/MyTask.java
+++ app/src/main/java/com/example/rsstt/MyTask.java
@@ -5,11 +5,13 @@ import android.util.Log;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.DocumentType;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class MyTask extends AsyncTask <String, Void, String> {
     final String BAD_URL="BAD_URL";
@@ -43,60 +45,93 @@ public class MyTask extends AsyncTask <String, Void, String> {
 
         if (doc!=null)
 
+
         {// запуск поиска RSS разные комбинации
-            result.add(getURLList(doc, doc.select("link"), "href", "rss"));// поиск в ленте
-            result.add(getURLList(doc, doc.select("link"), "href", "rss/news"));// поиск в ленте
-            result.add(getURLList(doc, doc.select("link"), "href", "rss2"));// поиск в ленте
-            result.add(getURLList(doc, doc.select("link"), "href", "rss2/news"));// поиск в ленте
-            result.add(getURLList(doc, doc.select("a"), "href", "feed"));// поиск в компютерре
-            result.add(getURLList(doc, doc.select("a"), "href", "feed/news"));// поиск в компютерре
-            result.add(getURLList(doc, doc.select("link"), "href", "feed"));// поиск в компютерре
-            result.add(getURLList(doc, doc.select("link"), "href", "feed/news"));// поиск в компютерре
-            result.add(getURLList(doc, doc.select("a"), "href", "rss"));// поиск в компютерре
+
+            result.addAll(createrAllLinkRss( doc, doc.select("link"), "href", "rss"));
+            result.addAll(createrAllLinkRss( doc, doc.select("link"), "href", "feed"));
+            result.addAll(createrAllLinkRss( doc, doc.select("a"), "href", "rss"));
+            result.addAll(createrAllLinkRss( doc, doc.select("a"), "href", "feed"));
+
+          /*  result.add(getURLList( doc, doc.select("link"), "href", "rss"));// поиск в ленте
+            result.add(getURLList( doc, doc.select("link"), "href", "rss/news"));// поиск в ленте
+            result.add(getURLList( doc, doc.select("link"), "href", "rss2"));// поиск в ленте
+            result.add(getURLList( doc, doc.select("link"), "href", "rss2/news"));// поиск в ленте
+            result.add(getURLList( doc, doc.select("a"), "href", "feed"));// поиск в компютерре
+            result.add(getURLList( doc, doc.select("a"), "href", "feed/news"));// поиск в компютерре
+            result.add(getURLList( doc, doc.select("link"), "href", "feed"));// поиск в компютерре
+            result.add(getURLList( doc, doc.select("link"), "href", "feed/news"));// поиск в компютерре
+            result.add(getURLList( doc, doc.select("a"), "href", "rss"));// поиск в компютерре
             result.add(getURLList(doc, doc.select("a"), "href", "rss/news"));// поиск в компютерре
+           */
+
             // поиск ссылок на фото
             resultPicture.add(getURLList(doc, doc.select("link"), "href", "favicon"));// поиск в ленте
             resultPicture.add(getURLList(doc, doc.select("link"), "href", "png"));// поиск в ленте
             titl = doc.title();
         }
-        else
-        Log.d("TAG", "EROR: ");
-        //for (int i = 0; i<result.size();i++) Log.d("TAG", "result " + result.get(i));
-       // Log.d("TAG", "result String " + getUrlRss(result));
-        //for (int i = 0; i<resultPicture.size();i++) Log.d("TAG", "result PICTURE " + resultPicture.get(i));
-       // Log.d("TAG", "TITLE   " + titl);
-       // формируем выходной списко
-        results.add(getUrlRss(result));
+
+
+        results.add(checerURL(result));
+
         results.add(titl);
         results.add(getUrlPicture(resultPicture));
         hearer.setData(results);
         return getUrlPicture(resultPicture);
     }
 // метод возвращает сылку на найденную  RSS ленту, принимает сам DOC, тег елемента - link, a, тег содержащий ссылку на RSS - href, и ключевое слов по которому ищем-RSS, feed
-    private String getURLList(Document doc, Elements title, String atr, String searchContains){
+    private String getURLList( Document doc, Elements title, String atr, String searchContains){
         String res=BAD_URL;
          for (Element link : title) {
                 if (link.attr(atr).contains(searchContains))
                 {String js = (link.attr(atr));
 
                  if(js.matches(".*(\\W|^)"+searchContains+"(\\W|$).*")|| js.matches(".*[ /]"+searchContains+"[ /].*"))
-                    res = (link.absUrl(atr));}
+                     res = (link.absUrl(atr));
+
+                    }
             }
 
         return res;
     }
-    private String getUrlRss(ArrayList<String> data){
-        ArrayList<String> list = data;
-        String result = MainActivity.mainactivityContext.getResources().getString(R.string.bad_url);
-        int checker=-1;
-        for (int i = 0; i<data.size(); i++){
-            if (data.get(i).contains("news")) checker = i;// запоминаем позицию самого ролевантного адреса
-            if (data.get(i).contains("rss")) checker = i;
-            if (data.get(i).contains("feed")) checker = i;
+
+    // метод создает массив ссылок на все похожее на RSS ленты
+    private ArrayList<String> createrAllLinkRss(Document doc, Elements title, String atr, String searchContains){
+        HashSet<String> res = new HashSet<>();
+        ArrayList<String> resList = new ArrayList<>();
+         String r="N";
+
+         for (Element link : title) {
+            if (link.attr(atr).contains(searchContains))
+             {res.add(link.absUrl(atr)); }
         }
-        if (checker==-1) return result;
-        return data.get(checker);
+       for(String str: res)
+        {resList.add(str);}
+        return  resList;
     }
+
+    // метод проверяет ссылки на ленты путем их загрузки и проверки соответствия заголовка
+    private String checerURL(ArrayList<String> list){
+
+        Document doc = null;//Здесь хранится будет разобранный html документ
+        for (String url:list) {
+            try {
+                doc = Jsoup.connect(url).get();
+            } catch (IOException e) {
+
+                e.printStackTrace();
+            }
+            if(doc!=null){
+            String el = doc.toString();
+            if (el.contains("<channel>")&&el.contains("<title>")&&el.contains("<link>") ) return url;
+            if (el.contains("rss")) return url;
+            }
+        }
+     return "BAD URL";
+
+    }
+
+
     private String getUrlPicture(ArrayList<String> data){
         ArrayList<String> list = data;
         String result = BAD_URL;
